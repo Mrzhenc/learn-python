d0b1e59298cba6f7d83edcbe7b7d0641f3cb6b71
zhenchao <mrzhencc@sina.com>
2017-10-20 11:17:34 +0800

    add web example

---
+++ myhttpd.py
@@ -0,0 +1,32 @@
+# encoding=utf8
+
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+
+class MyHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        try:
+            f = open(self.path[1:], 'r')
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(f.read())
+            f.close()
+        except IOError:
+            self.send_error(404, 'File Not Found: %s' % self.path)
+
+
+def main():
+    try:
+        server = HTTPServer(('http://', 80), MyHandler)
+        print 'Welcome to the machine...'
+        print "press ^c to exit"
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print "^c received .shutdown server"
+        server.socket.close()
+
+
+if __name__ == "__main__":
+    main()
